203c877b20244f5c127179e585bd314af2240ba8
Jesse <jessemckee97@gmail.com>
2024-04-24 09:12:27 -0500

    Quantity Argument

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     kotlin("jvm") version "1.3.40"
-    kotlin("kapt") version "1.3.40"
 }
 
 group = "me.zodd"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -16,13 +15,17 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    val sponge = create("org.spongepowered:spongeapi:7.4.0")
-    implementation(sponge)
-    kapt(sponge)
+    implementation("org.spongepowered:spongeapi:7.4.0")
 }
 
 tasks {
-  compileKotlin {
-      kotlinOptions.jvmTarget = "1.8"
-  }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+        targetCompatibility = "1.8"
+        sourceCompatibility = "1.8"
+    }
+    compileJava {
+        targetCompatibility = "1.8"
+        sourceCompatibility = "1.8"
+    }
 }
--- src/main/kotlin/plugin/Commands.kt
+++ src/main/kotlin/plugin/Commands.kt
@@ -31,24 +31,27 @@ class Commands {
     }
 
     private val argName: LiteralText = Text.of("name")
+    private val argQuantity: LiteralText = Text.of("quantity")
+    private val quantityArg: CommandElement =
+        GenericArguments.optional(GenericArguments.onlyOne(GenericArguments.integer(argQuantity)))
     private val nameArg: CommandElement = GenericArguments.onlyOne(GenericArguments.player(argName))
 
     val spec: CommandSpec = CommandSpec.builder()
         .description(Text.of("Sends an item to another user!"))
-        .arguments(nameArg)
+        .arguments(nameArg, quantityArg)
         .executor { src, args ->
             // Ensure sender is a player
             if (src !is Player) {
-                src.sendMessage(Text.of("Must be a player to use this command! Try /give?"))
+                src.sendMessage(err("Must be a player to use this command! Try /give?"))
                 return@executor CommandResult.empty()
             }
 
             // Target player being given an item
-            if (!args.hasAny("name")) {
+            if (!args.hasAny(argName)) {
                 src.sendMessage(err("Who do you want to send it to?"))
                 return@executor CommandResult.empty()
             }
-            val target = args.getOne<Player>("name").getOrNull()
+            val target = args.getOne<Player>(argName).getOrNull()
 
             if (target == null) {
                 src.sendMessage(err("You must use a valid player name!"))
@@ -68,33 +71,61 @@ class Commands {
                 return@executor CommandResult.empty()
             }
 
-            // Transaction result
-            val result = target.inventory.offer(item.createStack())
+            if (!args.hasAny(argQuantity)) {
+                // Transaction result
+                val stack = item.createStack()
+                val result = target.inventory.offer(stack)
 
-            if (result.type != InventoryTransactionResult.Type.SUCCESS) {
-                // Potentially something else could've gone wrong but this is lazy
-                src.sendMessage(err("Target players inventory is too full!"))
-                return@executor CommandResult.empty()
+                if (result.type != InventoryTransactionResult.Type.SUCCESS) {
+                    // Potentially something else could've gone wrong but this is lazy
+                    src.sendMessage(err("Target players inventory is too full!"))
+                    return@executor CommandResult.empty()
+                }
+
+                // Remove itemstack from Originating player
+                src.setItemInHand(HandTypes.MAIN_HAND, null)
+                notifyPlayers(src, target, item.type.name, item.quantity)
+            } else {
+                val amt = args.getOne<Int>(argQuantity).get()
+
+                if (amt < 1) {
+                    src.sendMessage(err("You must send a positive amount of items!"))
+                    return@executor CommandResult.empty()
+                }
+
+                val currentAmt = item.quantity
+
+                when {
+                    currentAmt == amt || currentAmt > amt -> {
+                        val stack = item.copy().createStack()
+                        stack.quantity = amt
+                        target.inventory.offer(stack)
+
+                        val ustack = item.copy().createStack()
+                        ustack.quantity -= amt
+
+                        src.setItemInHand(HandTypes.MAIN_HAND, ustack)
+                        notifyPlayers(src, target, item.type.name, amt)
+                    }
+                    else -> {
+                        src.sendMessage(Text.of("Not enough items! Try again with the correct amount!"))
+                        return@executor CommandResult.empty()
+                    }
+
+                }
             }
-
-            // Remove itemstack from Originating player
-            src.setItemInHand(HandTypes.MAIN_HAND, null)
-
-            val itemName = item.type.name
-            val itemQuantity = item.quantity
-
-            val sentItem: Text = succ("You have sent x$itemQuantity of $itemName to ${target.name}")
-            val receivedItem: Text = succ("You have received x$itemQuantity of $itemName from ${src.name}")
-
-            // Notify players of their items
-            src.sendMessage(sentItem)
-            target.sendMessage(receivedItem)
 
             CommandResult.success()
         }.build()
+
+    private fun notifyPlayers(p1: Player, p2: Player, name: String, sent: Int) {
+        val sentItem: Text = succ("You have sent x$sent of $name to ${p1.name}")
+        val receivedItem: Text = succ("You have received x$sent of $name from ${p2.name}")
+        p1.sendMessage(sentItem)
+        p2.sendMessage(receivedItem)
+    }
 }
 
 fun <T> Optional<T?>.getOrNull(): T? {
     return orElseGet(null)
 }
-
